433e66f7d0ee7f4bb44eeff12d525b60ed61560b
DESKTOP-4DH72C6\Huessin Sobhy <mohamedyamany055@gmail.com>
2025-08-11 13:08:32 +0300

    remove dvise preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:bike_app/bike_app.dart';
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => const BikeApp()));
-
-  //runApp(BikeApp());
+  runApp(BikeApp());
 }
